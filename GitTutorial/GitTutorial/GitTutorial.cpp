3f339a7269ad8dfb7b8541b02c80c04018f5a0c6
jerry12122 <a0975844268@gmail.com>
2021-03-05 11:52:43 +0800

    final

--- GitTutorial/GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -10,18 +10,18 @@ using namespace System;
 bool Guess(int number)
 {
     static int target = -1;
+
     if (target == -1) {
         Random r;
         target = r.Next() % 100 + 1;
     }
-    if (number > target) {
-        std::cout << "Smaller" << std::endl;
-        return false;
+    if (number == target) {
+        std::cout << "Correct";
+        target = -1;
+        return true;
     }
-    else if (number < target) {
-        std::cout << "Bigger" << std::endl;
-        return false;
-    }
+    else if (number > target) std::cout << "Smaller" << std::endl;
+    else if (number < target) std::cout << "Bigger" << std::endl;
     return false;
 }
 
