9e6c342186404735009dd1d4fb2a17a37287dcab
jerry12122 <a0975844268@gmail.com>
2021-06-25 09:44:14 +0800

    debug

--- game4.10/Source/mygame.cpp
+++ game4.10/Source/mygame.cpp
@@ -521,8 +521,8 @@ void CGameStateRun::OnMove()							// 移動遊戲元素
 		}
 	}
 	if (box.HitEraser(&player1)||box.HitEraser(&player2)) {
-		player2.SetFloor(box.GetX1(), box.GetY1());
-		player1.SetFloor(box.GetX1(), box.GetY1());
+		//player2.SetFloor(box.GetX1(), box.GetY1());
+		//player1.SetFloor(box.GetX1(), box.GetY1());
 		
 		box.SetMovingLeft(true);
 	}
@@ -919,9 +919,12 @@ void CGameStateRun2::OnBeginState()
 	reddoor.SetXY(34, 52);
 	icedoor.SetIsAlive(true);
 	icedoor.SetXY(106, 52);
+
 	background.SetTopLeft(0, 0);				// 設定背景的起始座標
-	// 設定說明圖的起始座標
+		// 設定說明圖的起始座標
 	//hits_left.SetInteger(HITS_LEFT);
+
+
 	hits_lake.SetInteger(HITS_LAKE);
 	hits_door.SetInteger(HITS_DOOR);
 	hits_left.SetTopLeft(HITS_LEFT_X, HITS_LEFT_Y);		// 指定剩下撞擊數的座標
